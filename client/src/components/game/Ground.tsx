21ff589658647c79c04c46739950c8c03767cd61
somethingrandomyouknow <121412095+somethingrandomyouknow@users.noreply.github.com>
2026-06-12 10:55:49 -0400

    Change ground texture to groundtexture.png

--- client/src/components/game/Ground.tsx
+++ client/src/components/game/Ground.tsx
@@ -3,7 +3,7 @@ import * as THREE from "three";
 
 export function Ground() {
   const basePath = import.meta.env.BASE_URL || '/';
-  const texture = useTexture(`${basePath}textures/grass.png`);
+  const texture = useTexture(`${basePath}textures/groundtexture.png`);
   
   texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
   texture.repeat.set(200, 200);
